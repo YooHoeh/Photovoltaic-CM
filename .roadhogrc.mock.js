6e3b51646b51284141b5a25445ebce04f8ba233b
YooHoeh <zako2810@sina.com>
2018-09-29 21:50:21 +0800

    移除无用组件

--- .roadhogrc.mock.js
+++ .roadhogrc.mock.js
@@ -29,26 +29,6 @@ const proxy = {
     },
   },
   // GET POST 可省略
-  'GET /api/users': [
-    {
-      key: '1',
-      name: 'John Brown',
-      age: 32,
-      address: 'New York No. 1 Lake Park',
-    },
-    {
-      key: '2',
-      name: 'Jim Green',
-      age: 42,
-      address: 'London No. 1 Lake Park',
-    },
-    {
-      key: '3',
-      name: 'Joe Black',
-      age: 32,
-      address: 'Sidney No. 1 Lake Park',
-    },
-  ],
   'GET /api/rule': getRule,
   'POST /api/rule': {
     $params: {
--- src/services/user.js
+++ src/services/user.js
@@ -1,9 +1,7 @@
 import request from '../utils/request';
 
-export async function query() {
-  return request('/api/users');
-}
 
 export async function queryCurrent() {
+  // return request('http://172.20.151.36/photovoltaic/public/index/users/info');
   return request('/api/currentUser');
 }
